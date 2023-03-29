1a926a5afdcadbc42e38ae615208dc28423c76cf
Javier Ortiz <javieros4@hotmail.com>
2023-03-28 20:59:38 -0600

    PrimerCommit

---
+++ models/server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const cors = require('cors');
+
+
+class Server {
+  constructor() {
+    this.app = express();
+    this.port = process.env.PORT;
+    this.userRouterPath= '/api/user';
+
+    //middlewares
+    this.middlewares();
+  
+    //rutas de aplicacion
+    this.routes();
+  }
+
+  middlewares() {
+    
+    //directorio publico
+    this.app.use(express.static("public"));
+    
+    //Lectura y parseo del body
+    this.app.use(express.json());
+    
+    //cors
+    this.app.use(cors());
+  }
+
+  routes() {
+   
+    this.app.use(this.userRouterPath, require('../routes/user.routes'));
+
+
+  }
+
+  listen() {
+    this.app.listen(this.port, () => {
+      console.log("Servidor corriendo en", this.port);
+    });
+  }
+}
+
+module.exports = Server;
